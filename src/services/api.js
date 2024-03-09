124063d2a860b1eae204ffe15eb4467c167819c9
its-me-ojas <crestfallenwizard@gmail.com>
2024-03-10 02:23:36 +0530

    baseURL changed

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://goprog.crestfallen.tech",
+  baseURL: "https://hackowasp.crestfallen.tech",
 });
 
 export const getAllTeams = () => api.get("/teamAll");
