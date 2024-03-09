98f68db9d3f53dfc0482e5c51313454e1db9c266
its-me-ojas <crestfallenwizard@gmail.com>
2024-03-09 23:57:15 +0530

    baseURL updated

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://goprog.crestfallen.tech",
 });
 
 export const getAllTeams = () => api.get("/teamAll");
